ed432ea931f0f3648954b4f4cb4ef0ec5f1b7e94
Dude <94567698+glit-hh-ch@users.noreply.github.com>
2023-05-29 00:37:18 +0300

    Update async_vk_album_downloader.py

--- async_vk_album_downloader.py
+++ async_vk_album_downloader.py
@@ -9,30 +9,7 @@ import time
 import os
 
 
-
 """ Не качает сохраненные фотографии """
-
-"""
-Порядок действий:
-1. Создаете vk приложение - https://vk.com/editapp?act=create
-2. Делаете зарос по ссылке ниже, вставив в строку id вашего приложения
-https://oauth.vk.com/authorize?client_id=ID_ВАШЕГО_ПРИЛОЖЕНИЯ&display=page&redirect_uri=https://oauth.vk.com/blank.html&scope=photos&response_type=token&v=5.52
-4. Из ответной строки поиска берете токен
-Ответная ссылка - https://oauth.vk.com/blank.html#access_token=vk1.a.EdQL_vPLxsbg4VNGvZJqh2ga-nvRUfvkNJSrH33x2s_7k672SVFxNQjnc1p4pktow-PYxJywQOMrZsZPfwy-mH6f0kTl7gQ5MAgHR1kxwkvr5Jr99NaYYRQuujbHd4R_dz4QczhGTs9yTaC-H6SVa0oxyoyauf5JRaDf8rX9BhmJZyJwkTfFzHcLHOsVh28E&expires_in=86400&user_id=123456789
-Где:
-Токен = vk1.a.EdQL_vPLxsbg4VNGvZJqh2ga-nvRUfvkNJSrH33x2s_7k672SVFxNQjnc1p4pktow-PYxJywQOMrZsZPfwy-mH6f0kTl7gQ5MAgHR1kxwkvr5Jr99NaYYRQuujbHd4R_dz4QczhGTs9yTaC-H6SVa0oxyoyauf5JRaDf8rX9BhmJZyJwkTfFzHcLHOsVh28E
-Время действия токена - 86400 сек (можно изменить, гуглите сами)
-ID юзера(ваш) - 123456789
-"""
-
-
-"""
-Получаем id человека:
-1. наживаем на любую фото и видим ссылку:
-    https://vk.com/albums123123123?z=photo123123123_457244488%2Fphotos123123123
-2. смотрим на photo123123123_457244488 id находиться между photo и _
-3. id человека это 123123123
-"""
 
 
 VK_TOKEN = ''

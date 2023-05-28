3192ae970846a723e13d84386d7db39ca655562c
Dude <94567698+glit-hh-ch@users.noreply.github.com>
2023-05-29 00:19:11 +0300

    Create async_vk_album_downloader.py

---
+++ async_vk_album_downloader.py
@@ -0,0 +1,139 @@
+import aiofiles
+import aiohttp
+import asyncio
+from aiohttp_retry import RetryClient
+
+import json
+import requests
+import time
+import os
+
+
+
+""" Не качает сохраненные фотографии """
+
+"""
+Порядок действий:
+1. Создаете vk приложение - https://vk.com/editapp?act=create
+2. Делаете зарос по ссылке ниже, вставив в строку id вашего приложения
+https://oauth.vk.com/authorize?client_id=ID_ВАШЕГО_ПРИЛОЖЕНИЯ&display=page&redirect_uri=https://oauth.vk.com/blank.html&scope=photos&response_type=token&v=5.52
+4. Из ответной строки поиска берете токен
+Ответная ссылка - https://oauth.vk.com/blank.html#access_token=vk1.a.EdQL_vPLxsbg4VNGvZJqh2ga-nvRUfvkNJSrH33x2s_7k672SVFxNQjnc1p4pktow-PYxJywQOMrZsZPfwy-mH6f0kTl7gQ5MAgHR1kxwkvr5Jr99NaYYRQuujbHd4R_dz4QczhGTs9yTaC-H6SVa0oxyoyauf5JRaDf8rX9BhmJZyJwkTfFzHcLHOsVh28E&expires_in=86400&user_id=123456789
+Где:
+Токен = vk1.a.EdQL_vPLxsbg4VNGvZJqh2ga-nvRUfvkNJSrH33x2s_7k672SVFxNQjnc1p4pktow-PYxJywQOMrZsZPfwy-mH6f0kTl7gQ5MAgHR1kxwkvr5Jr99NaYYRQuujbHd4R_dz4QczhGTs9yTaC-H6SVa0oxyoyauf5JRaDf8rX9BhmJZyJwkTfFzHcLHOsVh28E
+Время действия токена - 86400 сек (можно изменить, гуглите сами)
+ID юзера(ваш) - 123456789
+"""
+
+
+"""
+Получаем id человека:
+1. наживаем на любую фото и видим ссылку:
+    https://vk.com/albums123123123?z=photo123123123_457244488%2Fphotos123123123
+2. смотрим на photo123123123_457244488 id находиться между photo и _
+3. id человека это 123123123
+"""
+
+
+VK_TOKEN = ''
+VK_USER_ID = 123123123  # id человека у кого берете фото
+counter = 0  # просто счетчик
+
+if not os.path.exists('images'):
+    os.mkdir('images')
+
+
+def get_photo_data(offset=1, count=200) -> json.loads:
+    """
+    offset - значение озночающая сдвиг, пример: у вас 1300 фото, мах запрос 200, ставя offset на 1 вы получаете данные с 201 по 400, offset 2 с 401 по 600 и тд.
+    count - количество фото получаемое за раз мах 200
+    """
+    api = requests.get('https://api.vk.com/method/photos.getAll', params={
+        'owner_id': VK_USER_ID,
+        'access_token': VK_TOKEN,
+        'offset': offset,
+        'count': count,
+        'photo_sizes': 0,
+        'v': 5.103
+    })
+    return json.loads(api.text)
+
+
+async def check_name(filename) -> str:
+    """ проверка на содержание символов которые винда запрещает для имени файла при его записи """
+    bad = ['/', '\\', ':', '?', '"', '*', '<', '>', '|']
+    for bad_sim in bad:
+        if bad_sim in filename:
+            filename = filename.replace(str(bad_sim), '')
+
+    return filename
+
+
+async def get_photo(url, client_retry):
+    global counter
+    filename = await check_name(url.split('/')[-1])
+    async with aiofiles.open(f'images/{filename}.jpg', 'wb') as f:  # создаем файл и асинхронно работаем с ним
+        async with client_retry.get(url) as responce:  # делаем запрос на файл фото
+            async for chunck in responce.content.iter_chunked(5120):  # по кусочкам записывает файл
+                await f.write(chunck)
+    counter += 1
+    print(f'\r[+] Файлов {counter} записано', end='')
+
+
+async def gather_tasks(list_urls, client_retry):
+    """ т.к у VK ограничение по единовременному скачиванию файлов, сделал функцию для загрузок по 200 фото за раз"""
+    tasks = []
+    for url in list_urls:
+        task = asyncio.create_task(get_photo(url, client_retry))
+        tasks.append(task)
+
+    await asyncio.gather(*tasks)
+
+
+async def save_all_photos(list_of_list_urls):
+    async with aiohttp.ClientSession() as session:
+        client_retry = RetryClient(session)
+        for list_urls in list_of_list_urls:
+            await gather_tasks(list_urls, client_retry)
+
+
+async def get_photo_urls():
+    photos_urls = []
+
+    data = get_photo_data()
+    try:
+        count_photo = data['response']['count']
+    except:
+        print(data)
+        print('[+] Ошибка в обработке ответа, скорее всего время вашего токена истекло или вы вставили неправильный токен')
+        exit()
+
+    print('[+] Всего фото -', count_photo)
+
+    i = 0
+    count = 200  # количество файлов которые мы получим за раз, мах 200
+    while i <= count_photo:
+        data = get_photo_data(offset=i, count=count)
+        for files in data['response']['items']:
+            file_url = files['sizes'][-1]['url']
+            photos_urls.append(file_url)
+        i += count
+
+    print('[+] Получение всех ссылок -', len(photos_urls))
+
+    chunk_size = count
+    # создаем списки по количеству файлов которые мы получаем, мах 200 иначе появится ошибка
+    list_of_list_urls = [photos_urls[i:chunk_size + i] for i in range(0, len(photos_urls), chunk_size)]
+
+    await save_all_photos(list_of_list_urls)
+
+
+async def main():
+    await get_photo_urls()
+
+
+if __name__ == '__main__':
+    now = time.perf_counter()
+    asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
+    asyncio.run(main())
+    print(f'\n\n[+] Затрачено времени: {time.perf_counter() - now:.2f} сек')
